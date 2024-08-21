ffbe4022455240d407178f33026664f5cefba296
Baatarvant <baatarvant@gmail.com>
2024-08-21 12:01:22 +0800

    vercel deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,53 @@
+import { Skill } from "../components/Skill";
+import { JavascriptIcon } from "../components/icons/JavascriptIcon";
+import { TypescriptIcon } from "../components/icons/TypescriptIcon";
+
+const skills = [
+  {
+    id: 1,
+    name: "Javascript",
+    icon: <JavascriptIcon />,
+  },
+  {
+    id: 2,
+    name: "Javascript",
+    icon: <JavascriptIcon />,
+  },
+  {
+    id: 3,
+    name: "Javascript",
+    icon: <JavascriptIcon />,
+  },
+  {
+    id: 4,
+    name: "Javascrip",
+    icon: <TypescriptIcon />,
+  },
+  {
+    id: 5,
+    name: "Javascrip",
+    icon: <JavascriptIcon />,
+  },
+];
+
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-8">
+      {skills.map((skill) => {
+        const { id, ...other } = skill;
+        return (
+          <div key={id} className="justify-self-center">
+            <Skill {...other} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
+
+// es6
+const obj = {
+  a: 1,
+};
+
+const copyObj = { ...obj };
